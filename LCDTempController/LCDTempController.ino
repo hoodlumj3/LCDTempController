638fcb8093bd9b54a0abd80b9f4e4de278b5fef5
Matt Edwards <hoodlumj3@gmail.com>
2022-11-16 22:57:26 +0800

    Add project files.

---
+++ LCDTempController/LCDTempController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LCDTempController.ino
+ Created:	11/16/2022 10:57:15 PM
+ Author:	hoodlum
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
